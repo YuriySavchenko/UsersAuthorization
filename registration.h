26e5c9d30df0e40985ea59f9baef1d0bbdd59042
YuriySavchenko <yurec.proger@gmail.com>
2019-06-26 04:06:58 +0300

    class Registration which presents window for registration new users

---
+++ registration.h
@@ -0,0 +1,39 @@
+#ifndef REGISTRATION_H
+#define REGISTRATION_H
+
+#include <QWidget>
+#include <QMessageBox>
+
+namespace Ui {
+class Registration;
+}
+
+//!
+//! \brief The Registration class \n
+//! class \b Registration allows for \b users
+//! to create new \a accounts for next using
+//!
+class Registration : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Registration(QWidget *parent = nullptr);
+    ~Registration();
+
+private:
+    //!
+    //! \brief ui
+    //!
+    Ui::Registration *ui;
+
+private slots:
+    //!
+    //! \brief processRegistration \n
+    //! slot which allows write information
+    //! to \b DataBase about new \a user \n
+    //!
+    void processRegistration();
+};
+
+#endif // REGISTRATION_H
