9da467c74609ee748c7d0261dbbe00d171d6ea71
YuriySavchenko <yurec.proger@gmail.com>
2019-06-28 16:55:21 +0300

    added implementation for inserting data to DB after registration of user

--- registration.cpp
+++ registration.cpp
@@ -10,6 +10,9 @@ Registration::Registration(QWidget *parent) :
 
     // connection between slots and signals
     connect(ui->pushButtonSave, SIGNAL(clicked()), this, SLOT(processRegistration()));
+
+    // setting mode for input
+    this->ui->lineEditPassword->setEchoMode(QLineEdit::Password);
 }
 
 /* implementation of basic destructor */
@@ -28,10 +31,36 @@ void Registration::processRegistration()
         !ui->lineEditPassword->text().isEmpty() &&
         !ui->lineEditFirstName->text().isEmpty())
     {
-        if (ui->lineEditAge->text().toInt())
-            QMessageBox::warning(this, "Success", "Age is correct!");
+        if (ui->lineEditAge->text().toInt() and (ui->lineEditSex->text() == "male" or ui->lineEditSex->text() == "female")) {
+            QString insert = "INSERT INTO QtTest (login, password, firstname, lastname, sex, age) ";
+            QString values = "VALUES (";
+
+            values += "\'" + ui->lineEditLogin->text() + "\', ";
+            values += "\'" + ui->lineEditPassword->text() + "\', ";
+            values += "\'" + ui->lineEditFirstName->text() + "\', ";
+            values += "\'" + ui->lineEditLastName->text() + "\', ";
+            values += "\'" + ui->lineEditSex->text() + "\', ";
+            values += "\'" + ui->lineEditAge->text() + "\')";
+
+            QSqlDatabase db;
+            db = QSqlDatabase::addDatabase("QMYSQL");
+            db.setHostName("localhost");
+            db.setDatabaseName("yourdatabase");
+            db.setUserName("user");
+            db.setPassword("password");
+            db.open();
+
+            QSqlQuery query = QSqlQuery(db);
+
+            if (!query.exec(insert+values))
+                QMessageBox::critical(this, "DataBase Error", "Problem with DataBase!");
+            else {
+                this->close();
+            }
+        }
+
         else
-            QMessageBox::critical(this, "Input Error", "Age must be digit!");
+            QMessageBox::critical(this, "Input Error", "Please write only correct data!");
     }
 
     else {
