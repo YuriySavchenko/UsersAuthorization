f696f8f445c7415ed245fc7b6c6f97e33e5e53f3
YuriySavchenko <yurec.proger@gmail.com>
2019-06-28 16:53:16 +0300

    added implementation for selecting data from DB and viewing it for matched user

--- authorization.cpp
+++ authorization.cpp
@@ -10,6 +10,9 @@ Authorization::Authorization(QWidget *parent) :
 
     // connection between slots and signals
     connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(processAuthorization()));
+
+    // setting mode for input
+    this->ui->lineEditPassword->setEchoMode(QLineEdit::Password);
 }
 
 /* implementation of basic destructor */
@@ -22,7 +25,47 @@ Authorization::~Authorization()
 void Authorization::processAuthorization()
 {
     if (!ui->lineEditLogin->text().isEmpty() and !ui->lineEditPassword->text().isEmpty()) {
-        QMessageBox::warning(this, "Success", "User has found!");
+        QSqlDatabase db;
+        db = QSqlDatabase::addDatabase("QMYSQL");
+        db.setHostName("localhost");
+        db.setDatabaseName("yourdatabase");
+        db.setUserName("user");
+        db.setPassword("password");
+        db.open();
+
+        QSqlQuery query = QSqlQuery(db);
+
+        if (!query.exec("select * from QtTest"))
+            QMessageBox::critical(this, "DataBase Error", "Problem with DataBase!");
+
+        while (query.next()) {
+            QString login = query.value(1).toString();
+            QString password = query.value(2).toString();
+            QString firstName = query.value(3).toString();
+            QString lastName = query.value(4).toString();
+            QString sex = query.value(5).toString();
+            int age = query.value(6).toInt();
+
+            if (ui->lineEditLogin->text() == login && ui->lineEditPassword->text() == password) {
+                this->ui->lineEditFirstName->clear();
+                this->ui->lineEditLastName->clear();
+                this->ui->lineEditSex->clear();
+                this->ui->lineEditAge->clear();
+
+                this->ui->lineEditFirstName->insert(firstName);
+                this->ui->lineEditLastName->insert(lastName);
+                this->ui->lineEditSex->insert(sex);
+                this->ui->lineEditAge->insert(QString::number(age));
+                return;
+            }
+        }
+
+        QMessageBox::critical(this, "Authorization Error", "Login or password are incorrect!");
+
+        this->ui->lineEditFirstName->clear();
+        this->ui->lineEditLastName->clear();
+        this->ui->lineEditSex->clear();
+        this->ui->lineEditAge->clear();
     }
 
     else
